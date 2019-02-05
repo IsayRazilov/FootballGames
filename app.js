f908ee16c55a7321a6e05b53ab7716c745f8f01f
Isay Razilov <isay.razilov@solaredge.com>
2019-02-05 14:51:38 +0200

    added background

--- app.js
+++ app.js
@@ -10,17 +10,17 @@ var app = express();
 mongoose.connect("mongodb://aciles1221:qwerty1221@ds143474.mlab.com:43474/footballgames");
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
-
+app.use(express.static(__dirname + "/public"))
 
 app.get('/',function(req,res){
     res.render('home');
 });
 
-app.listen(3000,function(){
-    console.log("Server started at port 3000");
-});
-
-
-// app.listen(process.env.PORT, process.env.IP, function(){
-//     console.log("The YelpCamp Server Has Started!");
+// app.listen(3000,function(){
+//     console.log("Server started at port 3000");
 // });
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server is up");
+});

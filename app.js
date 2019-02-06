8512cd538997301c4e5d5c9c65d17dd966e54264
Isay Razilov <isay.razilov@solaredge.com>
2019-02-06 10:13:46 +0200

    adding mongo database

--- app.js
+++ app.js
@@ -7,14 +7,71 @@ var express        = require("express"),
 
 var app = express();
 
-mongoose.connect("mongodb://aciles1221:qwerty1221@ds143474.mlab.com:43474/footballgames");
+mongoose.connect("mongodb://aciles1221:qwerty1221@ds223605.mlab.com:23605/football_games");
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/public"))
 
-app.get('/',function(req,res){
-    res.render('home');
+
+app.get("/",function(req,res){
+    User.find({},function(err,users){
+        if(err) {
+            console.log(err) ;
+        } else {
+            res.render("home",{ users: users});
+        }
+    });
 });
+
+
+var user = new User({
+    username: "Isay", 
+    gamesPlayed: "30",
+    won: "15"
+});
+
+
+user.save(function(err,user){
+    if(err){
+        console.log("Error");
+    }else {
+        console.log("Added user");
+        console.log(user);
+    }
+})
+
+var user = new User({
+    username: "Aganya", 
+    gamesPlayed: "30",
+    won: "15"
+});
+
+
+user.save(function(err,user){
+    if(err){
+        console.log("Error");
+    }else {
+        console.log("Added user");
+        console.log(user);
+    }
+})
+
+
+var user = new User({
+    username: "Liron", 
+    gamesPlayed: "30",
+    won: "15"
+});
+
+
+user.save(function(err,user){
+    if(err){
+        console.log("Error");
+    }else {
+        console.log("Added user");
+        console.log(user);
+    }
+})
 
 // app.listen(3000,function(){
 //     console.log("Server started at port 3000");
